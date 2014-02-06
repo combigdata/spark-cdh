a737334e92e944ad3a082dbf9f201635c958e44f
Jenkins slave <kitchen-build@cloudera.com>
2014-02-06 13:21:45 -0800

    Preparing for CDH5.0.0b2 release

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -120,7 +120,7 @@ object SparkBuild extends Build {
 
   def sharedSettings = Defaults.defaultSettings ++ Seq(
     organization       := "org.apache.spark",
-    version            := "0.9.0-cdh5.0.0-beta-2-SNAPSHOT",
+    version            := "0.9.0-cdh5.0.0-beta-2",
     scalaVersion       := "2.10.3",
     scalacOptions := Seq("-Xmax-classfile-name", "120", "-unchecked", "-deprecation",
       "-target:" + SCALAC_JVM_VERSION),
