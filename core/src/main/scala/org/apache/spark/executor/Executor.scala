b754c14ab6778f25aaa33b5f015c7095e849953e
Kousuke Saruta <sarutak@oss.nttdata.co.jp>
2014-09-03 18:42:01 -0700

    [SPARK-3233] Executor never stop its SparnEnv, BlockManager, ConnectionManager etc.
    Author: Kousuke Saruta <sarutak@oss.nttdata.co.jp>
    
    Closes #2138 from sarutak/SPARK-3233 and squashes the following commits:
    
    c0205b7 [Kousuke Saruta] Merge branch 'SPARK-3233' of github.com:sarutak/spark into SPARK-3233
    064679d [Kousuke Saruta] Merge branch 'master' of git://git.apache.org/spark into SPARK-3233
    d3005fd [Kousuke Saruta] Modified Class definition format of BlockManagerMaster
    039b747 [Kousuke Saruta] Modified style
    889e2d1 [Kousuke Saruta] Modified BlockManagerMaster to be able to be past isDriver flag
    4da8535 [Kousuke Saruta] Modified BlockManagerMaster#stop to send StopBlockManagerMaster message when sender is Driver
    6518c3a [Kousuke Saruta] Merge branch 'master' of git://git.apache.org/spark into SPARK-3233
    d5ab19a [Kousuke Saruta] Merge branch 'master' of git://git.apache.org/spark into SPARK-3233
    6bce25c [Kousuke Saruta] Merge branch 'master' of git://git.apache.org/spark into SPARK-3233
    6058a58 [Kousuke Saruta] Modified Executor not to invoke SparkEnv#stop in local mode
    e5ad9d3 [Kousuke Saruta] Modified Executor to stop SparnEnv at the end of itself
    
    (cherry picked from commit 4bba10c41acaf84a1c4a8e2db467c22f5ab7cbb9)

--- core/src/main/scala/org/apache/spark/executor/Executor.scala
+++ core/src/main/scala/org/apache/spark/executor/Executor.scala
@@ -122,6 +122,9 @@ private[spark] class Executor(
     env.metricsSystem.report()
     isStopped = true
     threadPool.shutdown()
+    if (!isLocal) {
+      env.stop()
+    }
   }
 
   class TaskRunner(
