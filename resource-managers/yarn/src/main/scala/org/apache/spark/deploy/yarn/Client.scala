5537987d89a89d89e80d88a411ce0b736c80ee56
Marcelo Vanzin <vanzin@cloudera.com>
2015-07-21 11:10:01 -0700

    CDH-57798. Allow multiple paths in HADOOP_CONF_DIR.
    This allows us to append Hive configuration to the YARN configuration
    CM generates for Spark.
    
    (cherry picked from commit fadb73fa384a5239b95dc88f0659c38d21eb1cb1)
    (cherry picked from commit cb265791ac14b87e47461d3e08373a3b1d0ad0f4)
    (cherry picked from commit b2a1abacc551ee1258b4ea77ee965f2c37b69902)
    
    Change-Id: I80d03b21a1eef3a0626294bfd3b5e6804e97f1c9

--- resource-managers/yarn/src/main/scala/org/apache/spark/deploy/yarn/Client.scala
+++ resource-managers/yarn/src/main/scala/org/apache/spark/deploy/yarn/Client.scala
@@ -702,7 +702,7 @@ private[spark] class Client(
 
     if (sparkConf.getBoolean("spark.yarn.localizeConfig", true)) {
       confDirs.foreach { envKey =>
-        sys.env.get(envKey).foreach { path =>
+        sys.env.get(envKey).toSeq.flatMap(_.split(File.pathSeparator)).foreach { path =>
           val dir = new File(path)
           if (dir.isDirectory()) {
             val files = dir.listFiles()
