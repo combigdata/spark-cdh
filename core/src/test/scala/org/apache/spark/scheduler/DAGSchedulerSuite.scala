59f6e98c43a8ee62589e371a2c0f6cd6ad94b7ba
robbins <robbins@uk.ibm.com>
2015-09-04 15:23:29 -0700

    [SPARK-10454] [SPARK CORE] wait for empty event queue
    Author: robbins <robbins@uk.ibm.com>
    
    Closes #8605 from robbinspg/DAGSchedulerSuite-fix.
    
    (cherry picked from commit 2e1c17553d179f2d26a165805622cc01f92081b9)
    Signed-off-by: Andrew Or <andrew@databricks.com>
    (cherry picked from commit 09e08dbc15d470b546805d4c39ad78cb5f8e3837)

--- core/src/test/scala/org/apache/spark/scheduler/DAGSchedulerSuite.scala
+++ core/src/test/scala/org/apache/spark/scheduler/DAGSchedulerSuite.scala
@@ -803,6 +803,7 @@ class DAGSchedulerSuite
     }
 
     // The map stage should have been submitted.
+    sc.listenerBus.waitUntilEmpty(WAIT_TIMEOUT_MILLIS)
     assert(countSubmittedMapStageAttempts() === 1)
 
     complete(taskSets(0), Seq(
