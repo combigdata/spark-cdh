a4d86bf987f364a20c467c9df4b2324cf53333b9
Alex Bozarth <ajbozart@us.ibm.com>
2016-08-25 11:54:55 -0500

    [SPARK-15083][WEB UI] History Server can OOM due to unlimited TaskUIData
    This is a back port of #14673 addressing merge conflicts in package.scala that prevented a cherry-pick to `branch-2.0` when it was merged to `master`
    
    Since the History Server currently loads all application's data it can OOM if too many applications have a significant task count. This trims tasks by `spark.ui.retainedTasks` (default: 100000)
    
    Manual testing and dev/run-tests
    
    Author: Alex Bozarth <ajbozart@us.ibm.com>
    
    Closes #14794 from ajbozarth/spark15083-branch-2.0.
    
    (cherry picked from commit 55db26245d69bb02b7d7d5f25029b1a1cd571644)

--- core/src/main/scala/org/apache/spark/internal/config/package.scala
+++ core/src/main/scala/org/apache/spark/internal/config/package.scala
@@ -128,4 +128,10 @@ package object config {
       .internal()
       .stringConf
       .createWithDefaultString("AES/CTR/NoPadding")
+
+  // To limit memory usage, we only track information for a fixed number of tasks
+  private[spark] val UI_RETAINED_TASKS = ConfigBuilder("spark.ui.retainedTasks")
+    .intConf
+    .createWithDefault(100000)
+
 }
