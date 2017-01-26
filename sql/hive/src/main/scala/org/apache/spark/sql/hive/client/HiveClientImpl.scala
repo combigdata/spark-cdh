f4c0dda0b75ce3823ee49b9b7deac396b8b8efaf
Marcelo Vanzin <vanzin@cloudera.com>
2017-01-26 14:04:37 -0800

    CDH-49543. Ensure context class loader is set when initializing Hive.
    This makes sure classes are loaded by the correct class loader when
    all Hive internal state is being initialized.

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClientImpl.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClientImpl.scala
@@ -268,17 +268,19 @@ private[hive] class HiveClientImpl(
    */
   def withHiveState[A](f: => A): A = retryLocked {
     val original = Thread.currentThread().getContextClassLoader
-    // Set the thread local metastore client to the client associated with this HiveClientImpl.
-    Hive.set(client)
     // The classloader in clientLoader could be changed after addJar, always use the latest
     // classloader
     state.getConf.setClassLoader(clientLoader.classLoader)
+    Thread.currentThread().setContextClassLoader(clientLoader.classLoader)
+    // Set the thread local metastore client to the client associated with this ClientWrapper.
+    Hive.set(client)
     // setCurrentSessionState will use the classLoader associated
     // with the HiveConf in `state` to override the context class loader of the current
     // thread.
     shim.setCurrentSessionState(state)
     val ret = try f finally {
       Thread.currentThread().setContextClassLoader(original)
+      state.getConf.setClassLoader(original)
     }
     ret
   }
