0232372ec281d928ebe59baacdafeb02afcb7518
Mark Grover <mark@apache.org>
2017-03-06 10:40:20 -0800

    CDH-50815. [SPARK-19720][CORE] Redact sensitive information from SparkSubmit console
    This change redacts senstive information (based on `spark.redaction.regex` property)
    from the Spark Submit console logs. Such sensitive information is already being
    redacted from event logs and yarn logs, etc.
    
    Testing was done manually to make sure that the console logs were not printing any
    sensitive information.
    
    Here's some output from the console:
    
    ```
    Spark properties used, including those specified through
     --conf and those from the properties file /etc/spark2/conf/spark-defaults.conf:
      (spark.yarn.appMasterEnv.HADOOP_CREDSTORE_PASSWORD,*********(redacted))
      (spark.authenticate,false)
      (spark.executorEnv.HADOOP_CREDSTORE_PASSWORD,*********(redacted))
    ```
    
    ```
    System properties:
    (spark.yarn.appMasterEnv.HADOOP_CREDSTORE_PASSWORD,*********(redacted))
    (spark.authenticate,false)
    (spark.executorEnv.HADOOP_CREDSTORE_PASSWORD,*********(redacted))
    ```
    There is a risk if new print statements were added to the console down the road, sensitive information may still get leaked, since there is no test that asserts on the console log output. I considered it out of the scope of this JIRA to write an integration test to make sure new leaks don't happen in the future.
    
    Running unit tests to make sure nothing else is broken by this change.
    
    Author: Mark Grover <mark@apache.org>
    
    Closes #17047 from markgrover/master_redaction.
    
    (cherry picked from commit 5ae3516bfb7716f1793eb76b4fdc720b31829d07)

--- core/src/main/scala/org/apache/spark/util/Utils.scala
+++ core/src/main/scala/org/apache/spark/util/Utils.scala
@@ -34,6 +34,7 @@ import scala.io.Source
 import scala.reflect.ClassTag
 import scala.util.Try
 import scala.util.control.{ControlThrowable, NonFatal}
+import scala.util.matching.Regex
 
 import com.google.common.io.{ByteStreams, Files}
 import com.google.common.net.InetAddresses
@@ -2304,14 +2305,31 @@ private[spark] object Utils extends Logging {
   }
 
   private[util] val REDACTION_REPLACEMENT_TEXT = "*********(redacted)"
+  private val SECRET_REDACTION_PROPERTY = "spark.redaction.regex"
+  private val SECRET_REDACTION_DEFAULT = "(?i)secret|password"
 
   def redact(conf: SparkConf, kvs: Seq[(String, String)]): Seq[(String, String)] = {
-    val redactionPattern = conf.get("spark.redaction.regex", "(?i)secret|password").r
+    val redactionPattern = conf.get(SECRET_REDACTION_PROPERTY, SECRET_REDACTION_DEFAULT).r
+    redact(redactionPattern, kvs)
+  }
+
+  private def redact(redactionPattern: Regex, kvs: Seq[(String, String)]): Seq[(String, String)] = {
     kvs.map { kv =>
       redactionPattern.findFirstIn(kv._1)
-        .map { ignore => (kv._1, REDACTION_REPLACEMENT_TEXT) }
+        .map { _ => (kv._1, REDACTION_REPLACEMENT_TEXT) }
         .getOrElse(kv)
     }
+  }
+
+  /**
+   * Looks up the redaction regex from within the key value pairs and uses it to redact the rest
+   * of the key value pairs. No care is taken to make sure the redaction property itself is not
+   * redacted. So theoretically, the property itself could be configured to redact its own value
+   * when printing.
+   */
+  def redact(kvs: Map[String, String]): Seq[(String, String)] = {
+    val redactionPattern = kvs.getOrElse(SECRET_REDACTION_PROPERTY, SECRET_REDACTION_DEFAULT).r
+    redact(redactionPattern, kvs.toArray)
   }
 }
 
