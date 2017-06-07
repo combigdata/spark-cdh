5dd4437462b97a9f093f107b99ffa564d128887d
Reza Safi <rezasafi@cloudera.com>
2017-06-07 11:05:46 -0700

    CLOUDERA-BUILD. CDH-54554. Change minimum compatible CDH for spark 2.2 to 5.8 and maximum to 5.13
    Spark 2.2 is only supported for 5.8 =< CDH <= 5.13. This change builds spark2 parcel based on CDH5.8 and also updates the compatibility

--- cloudera/build_parcel.py
+++ cloudera/build_parcel.py
@@ -270,7 +270,7 @@ class Metadata(object):
                 "patchCount": str(self._version.patch_count()),
             },
             "replaces": "SPARK",
-            "depends" : "CDH (>= 5.8), CDH (<< 5.13)",
+            "depends" : "CDH (>= 5.8), CDH (<< 5.14)",
             "setActiveSymlink": True,
             "scripts": {
                 "defines": "spark2_env.sh",
