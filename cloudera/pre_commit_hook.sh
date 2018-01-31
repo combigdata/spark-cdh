6787f71b1c9b18f12b64af2dd8d32c71590073a0
Carl Myers <cmyers@cloudera.com>
2018-01-31 15:38:37 -0800

    RELENG-3066 - adjust precommit hook to use GBN dependencies
    Change-Id: I75433e9038e41da81dd522aa54be07e19f946a87

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -19,4 +19,14 @@ MVN_REPO_LOCAL=$HOME/.m2/repository${M2_REPO_SUFFIX}
 export MAVEN_OPTS="-XX:ReservedCodeCacheSize=512m"
 
 export APACHE_MIRROR=http://mirror.infra.cloudera.com/apache
-./build/mvn -B -Dcdh.build=true package -fae -Dmaven.repo.local="$MVN_REPO_LOCAL"
+
+# install mvn settings so dependencies come from a GBN of the latest build
+export CDH_GBN="$(curl "http://builddb.infra.cloudera.com:8080/query?product=cdh&version=6.x&user=jenkins&tag=official")"
+MVN_SETTINGS_FILE="$(mktemp)"
+function cleanup {
+  rm -f "$MVN_SETTINGS_FILE"
+}
+trap cleanup EXIT
+curl -L "http://github.mtv.cloudera.com/raw/CDH/cdh/cdh6.x/gbn-m2-settings.xml" > "$MVN_SETTINGS_FILE"
+
+./build/mvn -U -s "$MVN_SETTINGS_FILE" -B -Dcdh.build=true package -fae -Dmaven.repo.local="$MVN_REPO_LOCAL"
