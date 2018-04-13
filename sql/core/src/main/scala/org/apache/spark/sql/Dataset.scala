2b9e5fbd6ecb1b0d8ace4fe1d08bbd98b59d8017
Marcelo Vanzin <vanzin@cloudera.com>
2018-04-13 14:28:24 -0700

    [PYSPARK] Update py4j to version 0.10.7.
    (cherry picked from commit cc613b552e753d03cb62661591de59e1c8d82c74)
    Signed-off-by: Marcelo Vanzin <vanzin@cloudera.com>
    (cherry picked from commit 323dc3ad02e63a7c99b5bd6da618d6020657ecba)
    Signed-off-by: Marcelo Vanzin <vanzin@cloudera.com>
    (cherry picked from commit 850b7d868bbe23e58e1164fc551fac2facbebead)
    
    Cloudera ID: CDH-67227
    
    Change-Id: I047392c74211eebe7afcca5b780b12b0687daa68
    (cherry picked from commit 6b78c5562fb6066cc06b31ee2681a6daf51b68f8)

--- sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
@@ -2799,7 +2799,7 @@ class Dataset[T] private[sql](
     EvaluatePython.javaToPython(rdd)
   }
 
-  private[sql] def collectToPython(): Int = {
+  private[sql] def collectToPython(): Array[Any] = {
     EvaluatePython.registerPicklers()
     withNewExecutionId {
       val toJava: (Any) => Any = EvaluatePython.toJava(_, schema)
@@ -2809,7 +2809,7 @@ class Dataset[T] private[sql](
     }
   }
 
-  private[sql] def toPythonIterator(): Int = {
+  private[sql] def toPythonIterator(): Array[Any] = {
     withNewExecutionId {
       PythonRDD.toLocalIteratorAndServe(javaToPython.rdd)
     }
